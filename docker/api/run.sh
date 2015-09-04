60abe2b1f145aebfcac3d5d1c0d39723705abf83
Kostis Karantias <karantiaskostis@gmail.com>
2015-09-04 20:14:49 +0300

    Have api do a health check for the db
    Do this before running django. Until now, django started anyway and
    because the db container didn't have enough time to start, it crashed
    the whole api container.

--- docker/api/run.sh
+++ docker/api/run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+LOOP_LIMIT=60
+for (( i=0 ; ; i++ )); do
+    if [ ${i} -eq ${LOOP_LIMIT} ]; then
+        echo "=> Could not connect to the db container. Shutting down."
+        exit 1
+    fi
+    echo "=> Waiting for the db container to start up, trying ${i}/${LOOP_LIMIT}..."
+    sleep 1
+    mysql -hdb -uroot -pting -e "status" > /dev/null 2>&1 && break
+done
+
 python manage.py migrate
 python manage.py shell < /usr/src/runtime/create-default-channels.py
 python manage.py runserver 0.0.0.0:8000
