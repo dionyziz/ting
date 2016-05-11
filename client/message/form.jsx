b2a8b1c01ffc50383f5c897a1cc6b1cf1eaaba52
Hepic <hepic_@hotmail.com>
2016-05-11 23:52:16 +0300

    Build drag and drop client system

--- client/message/form.jsx
+++ client/message/form.jsx
@@ -6,6 +6,8 @@ const MessageForm = React.createClass({
     _MIN_UPDATE_WHEN_STOPPED: 500,
     _lastUpdate: 0,
     _lastUpdateTimeout: null,
+    _imageData: null,
+    _typeLastMessage: null,
     getInitialState() {
         return {
             message: ''
@@ -54,6 +56,26 @@ const MessageForm = React.createClass({
         }
         this.setState({message});
     },
+    onImageLoaded(event) {
+        this._imageData = event.target.result;
+        this.props.onStartTyping(this._imageData, 'image');
+    },
+    onStartTypingResponse(messageid) {
+        if (this._typeLastMessage == 'image') {
+            this.props.onMessageSubmit(this._imageData, 'image');
+        }
+    },
+    loadImage(src) {
+        var reader = new FileReader();
+        reader.onload = this.onImageLoaded;
+        reader.readAsDataURL(src);
+    },
+    handleDrop(event) {
+        event.preventDefault();
+        this._typeLastMessage = 'image';
+        var data = event.dataTransfer.files[0];
+        this.loadImage(data);
+    },
     render() {
         return (
             <div className='textarea'>
@@ -64,6 +86,7 @@ const MessageForm = React.createClass({
                            placeholder={i18n.t('messageInput.placeholder')}
                            value={this.state.message}
                            onChange={this.handleChange}
+                           onDrop={this.handleDrop}
                            ref='inputField' />
                 </form>
             </div>
