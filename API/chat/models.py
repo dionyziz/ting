238da6f5cb5409409f54980f4ce018fda897a766
Hepic <hepic_@hotmail.com>
2016-05-15 21:41:56 +0300

    Add message_type field into message model

--- API/chat/models.py
+++ API/chat/models.py
@@ -16,9 +16,20 @@ class Message(models.Model):
         serializable_fields = ('text', 'datetime_start', 'datetime_sent', 'username')
         return {key: getattr(self, key) for key in serializable_fields}
 
+    TEXT = 'text'
+    IMAGE = 'image'
+
+    MESSAGE_TYPE = (
+        (TEXT, 'text'),
+        (IMAGE, 'image'),
+    )
+
     text = models.TextField(max_length=2000)
     datetime_start = models.DateTimeField(default=None)
     datetime_sent = models.DateTimeField(default=None, null=True)
     typing = models.BooleanField(default=False)
     username = models.CharField(max_length=20)
     channel = models.ForeignKey(Channel)
+    message_type = models.CharField(max_length=10,
+                                    choices=MESSAGE_TYPE,
+                                    default=TEXT)
