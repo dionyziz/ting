948305f670a5b319eb1ca5fdaae1c0c9c476b166
Alexandros Kalimeris <kalimerisalx@gmail.com>
2016-07-06 22:11:59 +0300

    Add password setting

--- API/API/settings.py
+++ API/API/settings.py
@@ -29,6 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config['django']['secret_key']
 
+PASS = config['password']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config['django']['debug']
 
