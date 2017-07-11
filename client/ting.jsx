92fc9cfa9ec84f7de134e312e316358987486599
Hepic <hepic_@hotmail.com>
2017-07-11 22:44:17 +0300

    Add React-Router

--- client/ting.jsx
+++ client/ting.jsx
@@ -8,7 +8,10 @@ const UserList = require('./userlist.jsx'),
       i18n = require('i18next-client'),
       io = require('socket.io-client'),
       config = require('./config.jsx'),
-      _ = require('lodash');
+      _ = require('lodash'),
+      Route = require('react-router-dom').Route,
+      BrowserRouter = require('react-router-dom').BrowserRouter,
+      Switch = require('react-router-dom').Switch;
 
 const Ting = React.createClass({
     _socket: null,
@@ -163,6 +166,11 @@ i18n.init(
         lng: 'el-GR'
     },
     () => {
-        React.render(<Ting />, document.getElementsByClassName('ting')[0]);
+        ReactDOM.render((
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' component={Ting} />
+                </Switch>
+            </BrowserRouter>), document.getElementsByClassName('ting')[0]);
     }   
 );
